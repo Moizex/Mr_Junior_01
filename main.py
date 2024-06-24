200e2e8a0c179d847d029e0e1b8c7316c658d0c2
NawkzZ <kauanferreira344@gmail.com>
2024-06-24 01:26:35 -0400

    Musica, imagem de fundo e comentarios explicativos

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import pygame
-import pytmx
+
+# import pytmx
+
+# declarção de variáveis
+fps = 120
 
 
+# Criando Classe do Player
 class Player(pygame.sprite.Sprite):
+    # Criando função para os sprites e as variávies do player
     def __init__(self, groups):
         super().__init__(groups)
-        self.images_rigth = []
-        self.index = 0
-        self.counter = 0
-        for num in range(1, 18):
+        self.images_rigth = []  # Lista de sprites
+        self.index = 0  # Escolher número do sprite
+        # self.counter = 0 VÁRIAVEL DECLARADA E NÃO USADA
+        for num in range(1, 18):  # Loop para a mudança de imagens da lista
             img_right = pygame.image.load(f'imagens/boneco/boneco{num}.png')
-            self.images_rigth.append(img_right)
-        self.image = self.images_rigth[self.index]
+            self.images_rigth.append(img_right)  # append = acrescentar
+        self.image = self.images_rigth[self.index]  # frame atual (??)
         self.rect = self.image.get_frect(center=(window_width / 2, window_height / 2))
         self.direction = pygame.Vector2(1, 0)
         self.speed = 200  # velocidada que o boneco anda
-        self.vel_y = 0
+        self.vel_y = 0  # Velocidade de pulo e queda
         self.pulo = False
 
     def update(self, dt):
-        dx = 0
-        dy = 0
+        dx = 0  # DeltaX
+        dy = 0  # DeltaY
 
         # -- PULO ---
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.pulo == False and self.rect.bottom == window_height:
+            somPulo.play()#chamando som do pulo
             self.vel_y = -20  # altura do pulo
-            self.pulo = True
+            self.pulo = True  # limitar pulo infinito
 
         if not keys[pygame.K_SPACE]:
-            self.pulo = False
+            self.pulo = False  # limitar pulo infinito
 
+        # Não entendi
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
@@ -38,17 +46,17 @@ class Player(pygame.sprite.Sprite):
 
         # animation
         if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
-            self.index += 1
+            self.index += 1  # mudança de sprite, atualizando número da lista
             if self.index >= len(self.images_rigth):
-                self.index = 0
+                self.index = 0  # resetando index pra voltar o sprite
             self.image = self.images_rigth[self.index]
         if (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT] or self.pulo) == False:
-            self.index = 1
+            self.index = 1  # sprite inicial ao ficar parado
             self.image = self.images_rigth[self.index]
 
         # gravidade
-        self.vel_y += 1
-        if self.vel_y > 10:
+        self.vel_y += 1  # aceleração de queda
+        if self.vel_y > 10:  # velocidade máxima que ele cai
             self.vel_y = 10
         dy += self.vel_y
 
@@ -56,10 +64,10 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+        # colisão com a parte de baixo da tela
         if self.rect.bottom > window_height:
             self.rect.bottom = window_height
             dy = 0
-
 
 # class TiledMap:
 #     def __init__(self, filename):
@@ -83,18 +91,31 @@ class Player(pygame.sprite.Sprite):
 #         self.render(temp_surface)
 #         return temp_surface
 
-
+# iniciando módulos da classe pygame
 pygame.init()
-window_width, window_height = 1280, 720
-display_surface = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption('MrJunior01')
-running = True
-clock = pygame.time.Clock()
-fps = 24
+
+
+# Músicas usadas no jogo
+pygame.mixer.music.set_volume(1) #volume da musica (entre 0 e 1)
+mscFundo = pygame.mixer.music.load(f'Musicas\SuperMario.mp3')
+pygame.mixer.music.play(-1)#musica repetir pra sempre
+
+somPulo = pygame.mixer.Sound(f'Musicas\PuloMario.wav')
+
+window_width, window_height = 1280, 720  # definindo tamanho da janela
+display_surface = pygame.display.set_mode((window_width, window_height))  # criando superfície(tela)
+pygame.display.set_caption('MrJunior01')  # Nome da tela
+running = True  # boolean para ser usado no loop while
+clock = pygame.time.Clock()  #variável a ser usada pra definir o fps
+
+#Imagem de fundo
+imgFundo = pygame.image.load(f'imagens/Ambiente/ImagemFundo.png')
+imgFundo = pygame.transform.scale(imgFundo, [window_width +100, window_height+100])
+#tamanho ta maior pq png nao preenche fundo todo ai to adaptando
 
 # superfície
 surf = pygame.Surface((100, 200))
-surf.fill('gray')
+surf.fill('gray')  # cor da superficie
 x = 100
 
 all_sprites = pygame.sprite.Group()
@@ -114,7 +135,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    all_sprites.update(dt)
+    all_sprites.update(dt)  # mudança de sprite baseado no fps do jogo
 
     # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
     # if event.type == pygame.MOUSEMOTION:
@@ -129,7 +150,7 @@ while running:
     # player_rect.center += player_speed * player_direction * dt
 
     # desenhando o jogo
-    display_surface.fill('white')
+    display_surface.blit(imgFundo, (-50,-50)) #adaptando pra pegar o fundo
     all_sprites.draw(display_surface)
 
     # movimento do jogador
